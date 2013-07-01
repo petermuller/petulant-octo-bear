7fbf8f6996c69a57a445a9323666e13f3b2165d7
Peter Muller <pmm5983@rit.edu>
2013-07-01 00:06:17 -0400

    Changed to O-O style- code untested

--- main.py
+++ main.py
@@ -5,46 +5,11 @@ Includes the main program flow for the petulant-octo-bear program.
 
 @author Peter Muller (pmm5983@rit.edu)
 """
-
-def primeGen(primesList,numberToGen):
-    """
-    Generates the specified number of prime numbers.
-    Cycles through previously calculated prime numbers to check primeness of
-        the current number
-    
-    @param primesList - Current array of prime numbers
-    @param numberToGen - Number of prime numbers to generate
-    
-    @return pl - Updated array of prime numbers
-    
-    @pre - primesList is not empty
-    @pre - numberToGen is a whole number greater than 0.
-    """
-    pl = primesList
-    i = pl[-1] #current number; last number in current primes list
-    index = 0 #index of primes list
-    isPrime = True
-    while len(pl) < numberToGen:
-        while isPrime:
-            if index == len(pl):
-                break
-            if i % pl[index] == 0:
-                isPrime = False
-            index += 1
-        if isPrime:
-            pl.append(i)
-        isPrime = True
-        index = 0
-        i += 2
-    return pl
-    
-def getNthPrime(n):
-    return pl[n-1]
     
 def main():
-    #TODO convert this to O-O style.
-    primesList = [2,3]
-    pass
+    p = Prime()
+    #TODO - add code for gui
+    #TODO - add code for cli
     
 if __name__ == "__main__":
     main()
--- prime.py
+++ prime.py
@@ -4,4 +4,52 @@ prime.py
 The main class for collecting and generating prime number information
 """
 
-#TODO make class for prime number generator
+class Prime:
+    """
+    Class for generating and collecting prime numbers.
+    """
+    
+    def __init__(self):
+        self.primesList = [2,3] #known prime numbers to start the list
+    
+    def primeGen(self,numberToGen):
+        """
+        Generates the specified number of prime numbers.
+        Cycles through previously calculated prime numbers to check primeness of
+            the current number, then adds that number to the list if it is prime.
+        
+        @param numberToGen - Number of prime numbers to generate
+        
+        @pre - numberToGen is a whole number greater than 0.
+        @post - primesList contains at least numberToGen entries.
+        """
+        i = self.primesList[-1] #current number; last number in current primes list
+        index = 0 #index of primes list
+        isPrime = True
+        while len(self.primesList) < numberToGen:
+            while isPrime:
+                if index == len(self.primesList):
+                    break
+                if i % self.primesList[index] == 0:
+                    isPrime = False
+                index += 1
+            if isPrime:
+                self.primesList.append(i)
+            isPrime = True
+            index = 0
+            i += 2
+        return
+    
+    def getNthPrime(self,n)
+        """
+        Returns the Nth prime number. If it wasn't already calculated, the
+            method calls the primeGen method to calculate it.
+        
+        @param n - Index of desired prime number. Note: an n of 1 specifies the
+            first number in the list, so that translates to an index of 0 in
+            the primesList.
+            
+        @return number - the Nth prime number
+        """
+        primeGen(n) #if n < len(self.primesList): return
+        return self.primesList[n-1]
