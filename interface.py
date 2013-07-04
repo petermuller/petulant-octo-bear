657d9dfa2f25629e99e8becb43e7ab2a26f5efec
Peter Muller <pmm5983@rit.edu>
2013-07-03 22:36:38 -0400

    interface.py: GUI is now compatible with the isPrime method.

--- interface.py
+++ interface.py
@@ -45,36 +45,70 @@ class GUIInterface:
     def __init__(self,prime,master=None):
         #TODO cleanup, if possible?
         #TODO make windows more user-friendly.
+        
+        #main window initialization
         self.prime = prime
         master.title("Petulant-Octo-Bear")
         master.minsize(100,100)
-        self.input = Tkinter.Entry(master)
-        self.input.insert(Tkinter.INSERT, "Enter a number N for the Nth prime number")
-        self.input.pack(fill='x')
-        self.f = Tkinter.Frame(master)
-        self.f.pack()
-        self.submit = Tkinter.Button(self.f,text="Submit",command=self.submit)
-        self.submit.pack(side=Tkinter.LEFT)
-        self.ex = Tkinter.Button(self.f,text="Exit",command=self.quit)
+        
+        #Inputs
+        self.finputs = Tkinter.Frame(master)
+        self.finputs.pack()
+        self.input1 = Tkinter.Entry(self.finputs,width=40)
+        self.input1.insert(Tkinter.INSERT, "Enter a number N for the Nth prime number")
+        self.input2 = Tkinter.Entry(self.finputs,width=40)
+        self.input2.insert(Tkinter.INSERT, "Enter a number to check its primeness")
+        self.input1.pack(side=Tkinter.LEFT)
+        self.input2.pack(side=Tkinter.LEFT)
+        
+        #Buttons
+        self.fbuttons = Tkinter.Frame(master)
+        self.fbuttons.pack()
+        self.submit1 = Tkinter.Button(self.fbuttons,text="Find Nth Prime",command=self.submit1)
+        self.submit1.pack(side=Tkinter.LEFT)
+        self.ex = Tkinter.Button(self.fbuttons,text="Exit",command=self.quit)
         self.ex.pack(side=Tkinter.LEFT)
-        self.answer = Tkinter.Text(master,height=1)
-        self.answer.pack(fill='x')
+        self.submit2 = Tkinter.Button(self.fbuttons,text="Check Primeness",command=self.submit2)
+        self.submit2.pack(side=Tkinter.LEFT)
+        
+        #Results
+        self.fresults = Tkinter.Frame(master)
+        self.fresults.pack()
+        self.answer1 = Tkinter.Text(self.fresults,height=1,width=40)
+        self.answer1.pack(side=Tkinter.LEFT)
+        self.answer2 = Tkinter.Text(self.fresults,height=1,width=40)
+        self.answer2.pack(side=Tkinter.LEFT)
+        
         self.master = master
         
-    def submit(self):
+    def submit1(self):
         """
-        Gets input and sets the output
+        Tells what the Nth prime number is.
         """
+        self.answer1.delete('1.0','2.0')
         try:
-            number = int(self.input.get().strip())
+            number = int(self.input1.get().strip())
             if number < 1:
                 raise
-            self.answer.delete('1.0','2.0')
-            self.answer.insert(Tkinter.INSERT,str(self.prime.getNthPrime(number)))
+            self.answer1.insert(Tkinter.INSERT,str(self.prime.getNthPrime(number)))
         except:
-            self.answer.delete('1.0','2.0')
-            self.answer.insert(Tkinter.INSERT,"Error, please enter a positive integer.")
-        
+            self.answer1.insert(Tkinter.INSERT,"Error, please enter a positive integer.")
+    
+    def submit2(self):
+        """
+        Tells whether or not a number is prime
+        """
+        self.answer2.delete('1.0','2.0')
+        try:
+            number = int(self.input2.get().strip())
+            if self.prime.isPrime(number):
+                self.answer2.insert(Tkinter.INSERT,str(number)+" is a prime number!")
+            else:
+                self.answer2.insert(Tkinter.INSERT,str(number)+" is not a prime number!")
+        except:
+            self.answer2.insert(Tkinter.INSERT,"Error, please enter a positive integer.")
+                
+                
     def quit(self):
         """
         Exits the program
